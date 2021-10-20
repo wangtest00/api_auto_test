b7e8684418263d5b5addaa7c273b0439b66ce1b1
wangtest00 <370558913@qq.com>
2021-10-20 02:57:47 -0500

    1.增加2个渠道申请贷款接口 2.增加stp还款模拟银行回调

--- TestCase/lp_DaiHou_Api_Test.py
+++ TestCase/lp_DaiHou_Api_Test.py
@@ -79,6 +79,71 @@ class DaiHou_Api_Test(unittest.TestCase):
         t=r.json()
         print(t)
         self.assertEqual(t['errorCode'],0)
+        self.assertEqual(t['data']['loanStat'],'UNDER_RISK')
+        self.assertIsNotNone(t['data']['loanNo'])
+        self.assertIsNotNone(t['data']['custNo'])
+        self.assertIsNone(t['data']['bankAcctInfo'])
+        self.assertIsNone(t['data']['paymentDetail'])
+        self.assertIsNone(t['data']['trailPaymentDetail'])
+        self.assertIsNone(t['data']['repaymentDetail'])
+        self.assertIsNone(t['data']['reapplyDate'])
+        self.assertIsNone(t['data']['certStatus'])
+        self.assertIsNone(t['data']['applyButtonDetail'])
+    def test_fin_repay_stp(self):
+        '''【lanaPlus】/api/trade/fin/repay-STP申请还款接口-有在贷-正案例'''
+        registNo=cx_registNo_04()
+        phone=registNo[0]
+        custNo=registNo[1]
+        loanNo=registNo[2]
+        headt_api=login_code(phone)
+        data={"advance":"10000000","custNo":custNo,"defer":False,"loanNo":loanNo,"paymentMethod":"STP","repayInstNumList":["1"],"tranAppType":"Android"}
+        r=requests.post(host_api+"/api/trade/fin/repay",data=json.dumps(data),headers=headt_api,verify=False)
+        t=r.json()
+        self.assertEqual(t['errorCode'],0)
+        self.assertEqual(t['data']['code'],10000)
+        self.assertEqual(t['data']['msg'],'apply success')
+        self.assertEqual(t['data']['settle'],False)
+        self.assertEqual(t['data']['paymentMethod'],'STP')
+        self.assertEqual(t['data']['stpRepayment']['nombre'],'LANAPLUS')
+        self.assertEqual(t['data']['stpRepayment']['tipoDeCuenta'],'CLABE')
+        self.assertEqual(t['data']['stpRepayment']['destinatario'],'STP')
+        self.assertEqual(t['data']['stpRepayment']['concepto'],phone)
+        self.assertIsNone(t['data']['conektaRepayment'])
+    def test_fin_repay_oxxo(self):
+        '''【lanaPlus】/api/trade/fin/repay-OXXO申请还款接口-有在贷-正案例'''
+        registNo=cx_registNo_04()
+        phone=registNo[0]
+        custNo=registNo[1]
+        loanNo=registNo[2]
+        headt_api=login_code(phone)
+        data={"advance":"10000000","custNo":custNo,"defer":False,"loanNo":loanNo,"paymentMethod":"CONEKTA","repayInstNumList":["1"],"tranAppType":"Android"}
+        r=requests.post(host_api+"/api/trade/fin/repay",data=json.dumps(data),headers=headt_api,verify=False)
+        t=r.json()
+        self.assertEqual(t['errorCode'],0)
+        self.assertEqual(t['data']['code'],10000)
+        self.assertEqual(t['data']['msg'],'apply success')
+        self.assertEqual(t['data']['settle'],False)
+        self.assertEqual(t['data']['paymentMethod'],'CONEKTA')
+        self.assertIsNone(t['data']['stpRepayment'])
+        self.assertEqual(t['data']['conektaRepayment']['concepto'],phone)
+    def test_stp_repayment(self):
+        registNo=cx_registNo_05()
+        phone=registNo[0]
+        cuentaBeneficiario=registNo[1]
+        headt_api=login_code(phone)
+        r=requests.get(host_api+"/api/loan/latest/"+phone,headers=headt_api,verify=False)
+        t=r.json()
+        print('获取最近一笔贷款接口返回值=',t)
+        monto=t['data']['repaymentDetail']['repaymentDetailList'][0]['repaymentAmt']
+        loanNo=t['data']['loanNo']
+        data={"abono":{"id":"37755992","fechaOperacion":"20210108","institucionOrdenante":"40012","institucionBeneficiaria":"90646","claveRastreo":"MBAN01002101080089875109","monto":monto,
+                   "nombreOrdenante":"HAZEL VIRIDIANA RUIZ RICO               ","tipoCuentaOrdenante":"40","cuentaOrdenante":"012420028362208190","rfcCurpOrdenante":"RURH8407075F8","nombreBeneficiario":"STP                                     ",
+                   "tipoCuentaBeneficiario":"40","cuentaBeneficiario":cuentaBeneficiario,"rfcCurpBeneficiario":"null","conceptoPago":"ESTELA SOLICITO TRANSFERENCIA","referenciaNumerica":"701210","empresa":"QUANTX_TECH"}}
+        r=requests.post(host_pay+"/api/trade/stp_repayment/annon/event/webhook",data=json.dumps(data),headers=head_pay,verify=False)
+        t=r.json()
+        self.assertEqual(t['errorCode'],0)
+        afterstat=cx_afterstat(loanNo)
+        self.assertEqual(afterstat,'10270005')  #验证贷后状态是否更新为【已结清】
 
     @classmethod
     def tearDownClass(cls): #在所有用例都执行完之后运行的
