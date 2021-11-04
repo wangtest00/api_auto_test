f09a655eec33fee0cfae6397efdbcf4490d8addf
wangtest00 <370558913@qq.com>
2021-11-03 21:00:22 -0600

    1.1104优化更新,支持jenkins

--- Public/heads.py
+++ Public/heads.py
@@ -1,4 +1,6 @@
 from api_auto_test.public.var_fr import *
+import sys,io
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding="gb18030")
 
 def head_token(token):
     head={"user-agent": "Dart/2.12 (dart:io)","x-user-language": "es","accept-encoding": "gzip","content-length": "0","host_api": "test-api.quantx.mx","x-app-name": "LanaPlus","content-type": "application/json",
