e6f809a29372243551f555336db883573b7f5e92
wangtest00 <370558913@qq.com>
2021-12-13 23:27:43 -0600

    解决抽奖开关报错

--- TestCase/lp_DaiQian_Api_Test.py
+++ TestCase/lp_DaiQian_Api_Test.py
@@ -358,8 +358,10 @@ class LP_DaiQian_Api_Test(unittest.TestCase):
             self.assertEqual(t['data'],{"appNo":"201","funcType":"11100008","funcStat":True})
         elif config[0]==registNo[-1:] and jieqing_time>=config[1]:                # 手机号尾号等于配置尾号，已结清次数大于等于最小配置
             self.assertEqual(t['data'],{"appNo":"201","funcType":"11100008","funcStat":True})
-        elif config[0]!=registNo[-1:] and jieqing_time>=config[1]:                # 手机号尾号不等于配置尾号，已结清次数大于等于最小配置
+        elif registNo[-1:] in list(config[0]) and jieqing_time>=config[1]:                # 手机号尾号在配置尾号范围内，已结清次数大于等于最小配置
             self.assertEqual(t['data'],{"appNo":"201","funcType":"11100008","funcStat":True})
+        elif registNo[-1:] not in list(config[0]) and jieqing_time>=config[1]:            # 手机号尾号不在配置尾号范围内，已结清次数大于等于最小配置
+            self.assertEqual(t['data'],{'appNo':'201','funcType':'11100008','funcStat':False})
         else:
             self.assertEqual(t['data'],{'appNo':'201','funcType':'11100008','funcStat':False})
     @classmethod
