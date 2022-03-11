95e38ae2fea920f97a3b2c4f32032e84f12bce60
wangtest00 <370558913@qq.com>
2022-03-10 23:21:35 -0600

    1.引入XTestRunner美化测试报告

--- main_all.py
+++ main_all.py
@@ -1,6 +1,7 @@
 import unittest,os,io,sys
 from BeautifulReport import BeautifulReport as bf  #导入BeautifulReport模块，这个模块也是生成报告的模块，但是比HTMLTestRunner模板好看
 #from HTMLTestRunner_Chart import HTMLTestRunner
+from XTestRunner import HTMLTestRunner
 
 current_path=os.getcwd()  #获取当前路径
 case_path=os.path.join(current_path,"TestCase")
@@ -13,4 +14,17 @@ def load_all_case():
     return discover
 
 if __name__=='__main__':
-    bf(load_all_case()).report(filename='LP_FR_Api_Auto_Test_Report', description='LP_FR接口自动化测试')    #log_path='.'把report放到当前目录下
+    #bf(load_all_case()).report(filename='LP_FR_Api_Auto_Test_Report', description='LP_FR接口自动化测试')    #log_path='.'把report放到当前目录下
+    suit=load_all_case()
+    with(open('./LP_FR_Api_Auto_Test_Reports.html', 'wb')) as fp:
+        runner = HTMLTestRunner(
+            stream=fp,
+            title='LP_FR_Api_Auto_Test_Report',
+            description='LP_FR接口自动化测试',
+            language='en',
+        )
+        runner.run(
+            testlist=suit,
+            rerun=2,
+            save_last_run=False
+        )
